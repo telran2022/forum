297e655fc6c0b6cd5a7daa228f4968741e86e918
EddFish <edwardlfish@gmail.com>
2022-11-08 17:05:58 +0200

    Start develop auth filter

--- src/main/java/telran/java2022/accounting/controller/UserAccountController.java
+++ src/main/java/telran/java2022/accounting/controller/UserAccountController.java
@@ -1,10 +1,13 @@
 package telran.java2022.accounting.controller;
 
+import java.util.Base64;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,10 +30,13 @@ public class UserAccountController {
 		return accountService.addUser(userRegisterDto);
 	}
 
-//	@PostMapping("/login")
-//	public UserAccountResponseDto login() {
-//		
-//	}
+	@PostMapping("/login")
+	public UserAccountResponseDto login(@RequestHeader("Authorization") String token) {
+		String[] basicAuth = token.split(" ");
+		String decode = new String(Base64.getDecoder().decode(basicAuth[1]));
+		String[] credentials = decode.split(":");
+		return accountService.getUser(credentials[0]);
+	}
 
 	@DeleteMapping("/user/{login}")
 	public UserAccountResponseDto removeUser(@PathVariable String login) {
---
+++ src/main/java/telran/java2022/security/filter/AuthenticationFilter.java
@@ -0,0 +1,53 @@
+package telran.java2022.security.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import telran.java2022.accounting.dao.UserAccountRepository;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter implements Filter {
+	
+	final UserAccountRepository userAccountRepository;
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		if (checkEndPoint(request.getMethod(), request.getServletPath())) {
+			String token = request.getHeader("Authorization");
+			if(token == null) {
+				response.sendError(401);
+				return;
+			}
+			String[] credentials = getCredentialsFromToken(token);
+			// TODO authentication
+		}
+		chain.doFilter(request, response);
+	}
+
+	private String[] getCredentialsFromToken(String token) {
+		String[] basicAuth = token.split(" ");
+		String decode = new String(Base64.getDecoder().decode(basicAuth[1]));
+		String[] credentials = decode.split(":");
+		return credentials;
+	}
+
+	private boolean checkEndPoint(String method, String servletPath) {
+		return !("POST".equalsIgnoreCase(method) && servletPath.equals("/account/register"));
+	}
+
+}
